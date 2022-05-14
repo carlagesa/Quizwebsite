3d50e63d2c37b60e3426cb5ca7c9f9d75b4725b7
carlagesa <carltonmbaja0@gmail.com>
2022-05-14 07:51:34 +0300

    add student questions

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const quizData = 
+[
+    {
+        question: "Which of the following is a JavaScript framework",
+        a: "MariaDB",
+        b: "React",
+        c: "Perl",
+        d: "Django",
+        correct: 'b'
+    },
+    {
+        question: "Which of the following is not a language used in front-end development",
+        a: "PHP",
+        b: "HTML",
+        c: "CSS",
+        d: "Javascript",
+        correct: 'a'
+    },
+    {
+        question: "What is the most trending programming language?",
+        a: "Java",
+        b: "TypeScript",
+        c: "Python",
+        d: "PHP",
+        correct: 'c'
+    },
+    {
+        question: "When was ES6 released?",
+        a: "2015",
+        b: "2021",
+        c: "2014",
+        d: "None of the above",
+        correct: 'a'
+    },
+    {
+        question: "Full form of CSS",
+        a: "Casscading Some Sheets",
+        b: "Colloring Styling Sheets",
+        c: "Casscading Style Sheets",
+        d: "None of the above",
+        correct: 'c'
+    },
+    {
+        question: "Full form of HTML",
+        a: "HyperText Made Language",
+        b: "HyperText Markup Language",
+        c: "HyperCasual Terrific Language",
+        d: "None of these",
+        correct: 'b'
+    },
+    {
+        question: "What is NOT a JavaScript data types?",
+        a: "Strings",
+        b: "Floats",
+        c: "Integers",
+        d: "Decimals",
+        correct: "d"
+    }
+];
